94790893c9ac139ae4a99b3741c843887c93afc8
Konstantin Vlasov <konstantin.vlasov@rosalab.ru>
2013-06-25 15:18:48 +0400

    Added French translation

---
+++ lang/fr_FR.ts
@@ -0,0 +1,228 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+    <extra-po-header-language>fr</extra-po-header-language>
+    <extra-po-header-language_team>French (http://www.transifex.com/projects/p/rosa_comm_transl/language/fr/)</extra-po-header-language_team>
+    <extra-po-header-last_translator>sylvainsjc &lt;sylvainsjc@gmail.com&gt;</extra-po-header-last_translator>
+    <extra-po-header-po_revision_date>2013-06-25 10:14+0000</extra-po-header-po_revision_date>
+    <extra-po-header-project_id_version>ROSA Community Translation</extra-po-header-project_id_version>
+    <extra-po-header_comment>#
+# Translators:
+# sylvainsjc &lt;sylvainsjc@gmail.com&gt;, 2013</extra-po-header_comment>
+    <extra-po-headers>Project-Id-Version,PO-Revision-Date,Last-Translator,Language-Team,MIME-Version,Content-Type,Content-Transfer-Encoding,Language,Plural-Forms,X-Language,X-Qt-Contexts</extra-po-headers>
+<context>
+    <name>ImageWriter</name>
+    <message>
+        <location filename="../imagewriter.cpp" line="38"/>
+        <source>Failed to allocate memory for buffer:</source>
+        <translation>Impossible d&apos;allouer de la mémoire pour le tampon:</translation>
+    </message>
+    <message>
+        <location filename="../imagewriter.cpp" line="42"/>
+        <source>Failed to allocate memory for buffer.</source>
+        <translation>Impossible d&apos;allouer de la mémoire pour le tampon.</translation>
+    </message>
+    <message>
+        <location filename="../imagewriter.cpp" line="48"/>
+        <source>Failed to open the image file:</source>
+        <translation>Impossible d&apos;ouvrir le fichier image:</translation>
+    </message>
+    <message>
+        <location filename="../imagewriter.cpp" line="70"/>
+        <source>Failed to open the drive</source>
+        <translation>Impossible d&apos;ouvrir le lecteur</translation>
+    </message>
+    <message>
+        <location filename="../imagewriter.cpp" line="77"/>
+        <source>Failed to unmount the drive</source>
+        <translation>Impossible de démonter le disque</translation>
+    </message>
+    <message>
+        <location filename="../imagewriter.cpp" line="90"/>
+        <source>Failed to unmount the volume</source>
+        <translation>Impossible de démonter le volume</translation>
+    </message>
+    <message>
+        <location filename="../imagewriter.cpp" line="100"/>
+        <source>Failed to open the target device:</source>
+        <translation>Impossible d&apos;ouvrir le périphérique cible:</translation>
+    </message>
+    <message>
+        <location filename="../imagewriter.cpp" line="111"/>
+        <source>Failed to write to the device:</source>
+        <translation>Impossible d&apos;écrire sur le périphérique:</translation>
+    </message>
+    <message>
+        <location filename="../imagewriter.cpp" line="113"/>
+        <source>The last block was not fully written (%1 of %2 bytes)!
+Aborting.</source>
+        <translation>Le dernier bloc n&apos;a pas été entièrement écrit (%1 of %2 bytes)!
+Abandon.</translation>
+    </message>
+    <message>
+        <location filename="../imagewriter.cpp" line="132"/>
+        <source>Failed to read the image file:</source>
+        <translation>Impossible de lire le fichier image:</translation>
+    </message>
+</context>
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../maindialog.ui" line="105"/>
+        <source>&amp;USB Device:</source>
+        <translation>Périphérique &amp;USB:</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="124"/>
+        <source>Image:</source>
+        <translation>Image:</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="141"/>
+        <source>Open image file</source>
+        <translation>Ouvrir le fichier image</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="155"/>
+        <source>Refresh the list</source>
+        <translation>Actualiser la liste</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="192"/>
+        <source>%v / %m MB</source>
+        <translation>%v / %m MB</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="230"/>
+        <source>&amp;Write</source>
+        <translation>&amp;Ecrire</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="243"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Annuler</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="56"/>
+        <source>Failed to open the image file:</source>
+        <translation>Impossible d&apos;ouvrir le fichier image:</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="62"/>
+        <location filename="../maindialog.cpp" line="215"/>
+        <location filename="../maindialog.cpp" line="216"/>
+        <source>MB</source>
+        <translation>MB</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="141"/>
+        <location filename="../maindialog.cpp" line="152"/>
+        <source>Writing is in progress, abort it?</source>
+        <translation>L&apos;écriture est en cours, annuler?</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="161"/>
+        <source>Disk Images</source>
+        <translation>Images disque</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="161"/>
+        <source>All Files</source>
+        <translation>Tous les fichiers</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="214"/>
+        <source>The image is larger than your selected device!</source>
+        <translation>L&apos;image est plus grande que le périphérique sélectionné!</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="215"/>
+        <source>Image size:</source>
+        <translation>Taille de l&apos;image:</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="215"/>
+        <location filename="../maindialog.cpp" line="216"/>
+        <source>b</source>
+        <translation>b</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="216"/>
+        <source>Disk size:</source>
+        <translation>taille du disque:</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="224"/>
+        <source>Writing an image will erase all existing data on the selected device.
+Are you sure you wish to proceed?</source>
+        <translation>Graver une image va effacer toutes les données existantes sur le périphérique sélectionné.
+Êtes-vous sûr de vouloir continuer?</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="340"/>
+        <source>The operation completed successfully.</source>
+        <translation>L&apos;opération s&apos;est terminée avec succès.</translation>
+    </message>
+</context>
+<context>
+    <name>PhysicalDevice</name>
+    <message>
+        <location filename="../physicaldevice.cpp" line="37"/>
+        <source>Could not acquire lock:</source>
+        <translation>Impossible d&apos;acquérir le verrou:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../common.cpp" line="36"/>
+        <source>Error code:</source>
+        <translation>Code d&apos;erreur:</translation>
+    </message>
+    <message>
+        <location filename="../usbdevice.h" line="15"/>
+        <source>Unknown Device</source>
+        <translation>Périphérique inconnu</translation>
+    </message>
+    <message>
+        <location filename="../usbdevice.h" line="24"/>
+        <source>&lt;unmounted&gt;</source>
+        <translation>&lt;démonté&gt;</translation>
+    </message>
+    <message>
+        <location filename="../usbdevice.h" line="24"/>
+        <source>MB</source>
+        <translation>MB</translation>
+    </message>
+    <message>
+        <location filename="../platform_win.cpp" line="41"/>
+        <source>Memory allocation for %1 failed.</source>
+        <translation>L&apos;allocation de mémoire pour %1 a échoué.</translation>
+    </message>
+    <message>
+        <location filename="../platform_win.cpp" line="89"/>
+        <source>CoCreateInstance(WbemAdministrativeLocator) failed.</source>
+        <translation>CoCreateInstance(WbemAdministrativeLocator) échoué.</translation>
+    </message>
+    <message>
+        <location filename="../platform_win.cpp" line="90"/>
+        <source>ConnectServer failed.</source>
+        <translation>ConnectServer échoué.</translation>
+    </message>
+    <message>
+        <location filename="../platform_win.cpp" line="91"/>
+        <source>Failed to query USB flash devices.</source>
+        <translation>Impossible d&apos;obtenir les périphériques flash USB.</translation>
+    </message>
+    <message>
+        <location filename="../platform_win.cpp" line="155"/>
+        <source>Failed to query list of partitions.</source>
+        <translation>Impossible d&apos;obtenir la liste des partitions.</translation>
+    </message>
+    <message>
+        <location filename="../platform_win.cpp" line="187"/>
+        <source>Failed to query list of logical disks.</source>
+        <translation>Impossible d&apos;obtenir la liste des disques logiques.</translation>
+    </message>
+</context>
+</TS>
