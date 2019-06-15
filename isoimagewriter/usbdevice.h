e85a918d25dc0ea95ebc89bb2ec40c7935dffbd6
Farid Boudedja <farid.boudedja@gmail.com>
2019-06-15 15:40:43 +0100

    Use human-readable format for USB device size

--- isoimagewriter/usbdevice.h
+++ isoimagewriter/usbdevice.h
@@ -25,6 +25,7 @@
 #include <QStringList>
 
 #include <KLocalizedString>
+#include <KFormat>
 
 class UsbDevice
 {
@@ -39,7 +40,8 @@ public:
     // Formats the device description for GUI
     // The format is: "<volume(s)> - <user-friendly name> (<size in megabytes>)"
     QString formatDisplayName() const {
-        return ((m_Volumes.size() == 0) ? i18n("<unmounted>") : m_Volumes.join(", ")) + " - " + m_VisibleName + " (" + QString::number(alignNumberDiv(m_Size, DEFAULT_UNIT)) + " " + i18n("MiB") + ")";
+        return ((m_Volumes.size() == 0) ? i18n("<unmounted>")
+                : m_Volumes.join(", ")) + " - " + m_VisibleName + " (" + KFormat().formatByteSize(m_Size) + ")";
     }
 
     // User-friendly name of the device
