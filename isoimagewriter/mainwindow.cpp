c29cecc8eb4b85e17432b42945543566cc31a7fe
Jonathan Esk-Riddell <jr@jriddell.org>
2022-04-13 14:53:31 +0100

    Revert "force use of kicontheme in the hope this makes use of initRCCIconTheme() in Windows"
    This reverts commit c7a6ebcfa9f951f23561690c152793c09092ace0.

--- isoimagewriter/mainwindow.cpp
+++ isoimagewriter/mainwindow.cpp
@@ -28,7 +28,6 @@
 #include <KIconLoader>
 #include <KPixmapSequence>
 #include <KLocalizedString>
-#include <KIconTheme>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
@@ -37,7 +36,6 @@ MainWindow::MainWindow(QWidget *parent)
       m_enumFlashDevicesWaiting(false),
       m_externalProgressBar(this)
 {
-    qDebug() << KIconTheme::current();
     setupUi();
     setAcceptDrops(true);
 
